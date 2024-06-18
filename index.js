ba386b4483dd00e8d7975f6420358ae63b987484
Benjamin <benjamindeltour22@gmail.com>
2024-06-18 11:16:57 +0200

    have test that work

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    console.log(`App running on port http://localhost:${port}`);
+  });
+}
 
+module.exports = app;
